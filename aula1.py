e5570291cfcfd88839cd23f9503c72f22c6071c6
Analuiza15 <analuizabarrozo08@gmail.com>
2024-08-19 11:41:15 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,10 @@
+nome = "Ana"
+idade = 16
+
+print (" Olá ", nome, "a sua idade é", idade )
+
+idade = 30
+nome = "José"
+altura = 1.74
+
+print ("Olá ", nome, " a sua idade é ", idade, "a sua altura é", altura)
---
+++ media.py
@@ -0,0 +1,8 @@
+# Algoritimo média: criar um algoritimo que leia 4 nota e apresente a média ao final
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print ("A média final obitida:", media)
