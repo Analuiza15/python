abf57f096a0f479698c08ad31288a85db5d28a94
Analuiza15 <analuizabarrozo08@gmail.com>
2024-09-11 11:48:41 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,10 @@
+def soma (a, b):
+    return a + b
+
+
+valorA = int(input("Digite um valor: "))
+valorB = int(input("Digite um valor: "))
+
+resultado = soma(valorA, valorB)
+
+print(f"O resultado da soma é {resultado}")
